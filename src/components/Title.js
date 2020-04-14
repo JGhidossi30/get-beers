18fde56048af16303407a5d6410aa66d77588ef0
Jordan Ghidossi <ghidossi@usc.edu>
2020-04-14 14:55:00 -0700

    Added Link to Title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,14 @@
 import React from "react";
 import './Title.css';
+import {Link} from "react-router-dom";
 
 const Title = () => (
     <>
         <div className="title">
             <h1>
-                The Beer Garden
+                <Link to="/">
+                    The Beer Garden
+                </Link>
             </h1>
         </div>
     </>
